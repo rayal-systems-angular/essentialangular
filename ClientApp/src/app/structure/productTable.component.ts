415afeb7467917b756bf20a9f4243caae0d99f74
Pankaj Rayal <pankajrayal@gmail.com>
2020-06-05 08:40:35 +0530

    CH07-Complete

---
+++ ClientApp/src/app/structure/productTable.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Repository } from '../models/repository';
+import { Product } from '../models/product.model';
+
+@Component({
+  selector: 'product-table',
+  templateUrl: './productTable.component.html',
+})
+export class ProductTableComponent {
+  constructor(private repo: Repository, private router: Router) {}
+
+  get products(): Product[] {
+    return this.repo.products;
+  }
+
+  selectProduct(id: number) {
+    this.repo.getProduct(id);
+    this.router.navigateByUrl('/detail');
+  }
+}
